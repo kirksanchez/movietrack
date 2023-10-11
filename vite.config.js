3d315f6664971ecd044ab28ac3ce22d7fc96c7c3
Kirk Sanchez <kirksanchez01@gmail.com>
2023-10-11 21:44:28 +0800

    fix: edit proxy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      api: 'https://movietrack-database.netlify.app',
+      api: 'https://movietrack-database.netlify.app/home',
     },
   },
   plugins: [react()],
